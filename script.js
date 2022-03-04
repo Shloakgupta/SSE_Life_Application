6ef16c043540b8d1ae508c6b1f00975d2bfa2aca
Shloak Gupta <shloakgupta09@gmail.com>
2022-03-03 19:35:24 -0600

    Update

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function changeTodo(button_item) {
     button_item.innerText = "Todo"
   }
 }
+
+function reset() {
+  let buttons = document.getElementsByTagName('button')
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].classList[2] == 'btn-success') {
+      buttons[i].classList.remove('btn-success')
+      buttons[i].innerText = "Todo"
+    }
+  }
+}
